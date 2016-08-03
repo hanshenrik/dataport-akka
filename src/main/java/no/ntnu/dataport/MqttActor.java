2a7e701c40a302c87e76e5fa89f7949c5eb05069
hanshenrik <hh.gronsleth@gmail.com>
2016-08-03 17:26:00 +0200

    Refactor, add MQTT options, don't publish to topic by default

--- src/main/java/no/ntnu/dataport/MqttActor.java
+++ src/main/java/no/ntnu/dataport/MqttActor.java
@@ -10,23 +10,24 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import java.util.Date;
 
-public class TTNActor extends UntypedActor implements MqttCallback {
+public class MqttActor extends UntypedActor implements MqttCallback {
     LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
     /**
      * Create Props for an actor of this type.
-     * @param broker The broker address to be passed to this actor’s constructor.
-     * @param topic The topic the actor should subscribe to.
-     * @param qos The quality of service requirements for the MQTT connection.
+     * @param broker    The broker address to be passed to this actor’s constructor.
+     * @param topic     The topic the actor should subscribe to.
+     * @param qos       The quality of service requirements for the MQTT connection.
      * @return a Props for creating this actor, which can then be further configured
      *         (e.g. calling `.withDispatcher()` on it)
      */
     public static Props props(final String broker, final String topic, final int qos) {
-        return Props.create(new Creator<TTNActor>() {
+        return Props.create(new Creator<MqttActor>() {
             private static final long serialVersionUID = 1L;
 
             @Override
-            public TTNActor create() throws Exception {
-                return new TTNActor(broker, topic, qos);
+            public MqttActor create() throws Exception {
+                return new MqttActor(broker, topic, qos);
             }
         });
     }
@@ -37,13 +38,14 @@ public class TTNActor extends UntypedActor implements MqttCallback {
     final String clientId;
     final String content;
 
-    public TTNActor(String broker, String topic, int qos) {
+    public MqttActor(String broker, String topic, int qos) {
         log.info("Constructor called with broker: {}, topic: {}, qos: {}", broker, topic, qos);
         this.broker     = broker;
         this.topic      = topic;
         this.qos        = qos;
         this.clientId   = "client-" + new Date().getTime();
         this.content    = "TTN actor started successfully!";
+
         MemoryPersistence persistence = new MemoryPersistence();
         try {
             MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
@@ -51,17 +53,20 @@ public class TTNActor extends UntypedActor implements MqttCallback {
             // Set the callback function to be able to receive messages.
             sampleClient.setCallback(this);
 
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
+            MqttConnectOptions connectionOptions = new MqttConnectOptions();
+            connectionOptions.setCleanSession(true);
+            // TODO: take these as (optional?) parameters. Needed for staging.thethingsnetwork.org!
+//            connectionOptions.setUserName("70B3D57ED000091D");
+//            connectionOptions.setPassword("ytpiqGLn5fYjlmkMvC95sim02Je6BeB+7dxn4MU4/Bg=".toCharArray());
 
             log.info("Connecting to broker: {}", broker);
-            sampleClient.connect(connOpts);
+            sampleClient.connect(connectionOptions);
 
             sampleClient.subscribe(topic);
             log.info("Subscribed to topic: {}", topic);
 
-            sampleClient.publish(topic, new MqttMessage(content.getBytes()));
-            log.info("Published to topic: {}", topic);
+//            sampleClient.publish(topic, new MqttMessage(content.getBytes()));
+//            log.info("Published to topic: {}", topic);
         } catch(MqttException me) {
             log.info("reason "+me.getReasonCode());
             log.info("msg "+me.getMessage());
