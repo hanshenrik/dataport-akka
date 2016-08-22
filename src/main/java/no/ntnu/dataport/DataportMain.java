ffe6bc5ca1e5de8e8dc59b6db8fc2d98b97ced66
hanshenrik <hh.gronsleth@gmail.com>
2016-08-22 12:54:04 +0200

    Remove unused classes

--- src/main/java/no/ntnu/dataport/DataportMain.java
+++ src/main/java/no/ntnu/dataport/DataportMain.java
@@ -19,23 +19,8 @@ public class DataportMain {
         Props externalResourceSupervisorProps = Props.create(ExternalResourceSupervisorActor.class);
         final ActorRef externalResourceSupervisor = system.actorOf(externalResourceSupervisorProps, "externalResourceSupervisor");
 
-        // Create the "actor-in-a-box"
-        //final Inbox inbox = Inbox.create(system);
-
-        // Tell the 'greeter' to change its 'greeting' message
-        //greeter.tell(new WhoToGreet("akka"), ActorRef.noSender());
-
-        // Ask the 'greeter for the latest 'greeting'
-        // Reply should go to the "actor-in-a-box"
-        //inbox.send(greeter, new Greet());
-        //inbox.send(ttn, new MqttConnectMessage());
-
         final ActorRef trondheim = system.actorOf(SiteActor.props("trondheim", 63.430515, 10.395053), "site-trondheim");
         final ActorRef vejle = system.actorOf(SiteActor.props("vejle", 55.711311, 9.536354), "site-vejle");
-
-//        final ActorRef subscriber1 = system.actorOf(Props.create(Subscriber.class), "subscriber1");
-//        final ActorRef publisher1 = system.actorOf(Props.create(Publisher.class), "publisher1");
-//        publisher1.tell("hello", null);
     }
 
 }
